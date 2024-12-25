8192abb66a63c90efde78e0b5ed941d83d58f55d
Nikita Stepanov <nst175@gmail.com>
2024-12-25 20:12:45 +0100

    1

--- Otus_lesson_18/Program.cs
+++ Otus_lesson_18/Program.cs
@@ -1,24 +1,18 @@
 ﻿namespace Otus_lesson_18;
 
-// Предмет в игре может обладать следующими свойствами:
-// Consumable
-// Equipable
-// QuestItem
-// Tradeable
-// Legendary
-//
-// У персонажа может быть несколько предметов
-// У нашего первого персонажа будут Equipable и Legendary меч, Equipable и QuestItem амулет и Tradeable зелье
+// Реализуйте структуры Point, Circle и Rectangle.
+// Добавьте методы для вычисления площади кругов и прямоугольников.
+// Выведите информацию о созданных фигурах и их площади в консоль.
 
 class Program
 {
     static void Main(string[] args)
     {
-        // var point = new Point(10, 20);
-        // var circle = new Circle(15, 20);
-        // var rectangle = new Rectangle(10, 20, 30);
-        //
-        // Console.WriteLine(point);
+        var point = new Point(10, 20);
+        var circle = new Circle(15, 20);
+        var rectangle = new Rectangle(10, 20, 30);
+
+        Console.WriteLine(point);
         
     }
 }
